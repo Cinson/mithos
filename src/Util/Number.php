283f2cea1a196e033e1873dcfdad286542b10391
hernandes.flavio@gmail.com <hernandes.flavio@gmail.com>
2015-05-13 12:04:27 -0300

    Helper para converter números monetários em decimal

--- src/Util/Number.php
+++ src/Util/Number.php
@@ -7,5 +7,8 @@ class Number {
     public static function format($number, $decimals = 0, $sep = '.', $thousand = '.') {
         return number_format($number, $decimals, $sep, $thousand);
     }
-    
+
+    public function toFloat($value) {
+        return preg_replace('/^([0-9]+)(\.([0-9]+))?,([0-9]+)$/', '$1$3.$4', $value);
+    }
 }
